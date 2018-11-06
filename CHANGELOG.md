bf07e85d45d4503357d95a5b7ea539e44146dcd2
Tim Gurney aka Wolf <wolf@tgwolf.com>
2018-11-06 13:51:42 +0000

    Fix a couple of linting errors in the new version code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 1.0.1 (November 6, 2018)
+
+CHANGES:
+
+* Improve the version checking so that a version is checked to ensure it is an actual upgrade and not just a different version. ([@TGWolf][])
+
 ## 1.0.0 (November 6, 2018)
 
 * Initial Release ([@TGWolf][])
--- src/rds/check-for-rds-upgrade/check-for-rds-upgrade.py
+++ src/rds/check-for-rds-upgrade/check-for-rds-upgrade.py
@@ -27,8 +27,9 @@ def cmp_version(version1, version2):
     """
 
     def normalize(v):
-        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+        return [int(x) for x in re.sub(r'(\.0+)*$', '', v).split(".")]
     return cmp(normalize(version1), normalize(version2))
+
 
 def main(cmdline=None):
 
